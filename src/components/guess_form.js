097f827d7bd9900e7925caf7ec4ca727178a5a73
David S. Jackson <deepbsd@yahoo.com>
2017-09-09 18:54:58 -0400

    trying something else on guess_form

--- src/components/guess_form.js
+++ src/components/guess_form.js
@@ -7,30 +7,32 @@ export default class GuessForm extends React.Component {
 			super(props);
 			this.passIt = this.passIt.bind(this);
 			this.state = {
-					userGuess: ''
+					userGuess: '',
+					resetVal: ''
 
 			}
 	}
 
-	resetForm(e){
-		e.preventDefault();
+	resetForm(){
 		this.setState({
-			userGuess: ''
-		})
+			resetVal: ''
+		});
 	}
 
 	passIt(input){
 		this.setState({
-			userGuess: input
+			userGuess: input,
+			resetVal: input
 		})
-
+		this.resetForm();
 	}
 
 render(){
+	let inVal = this.state.resetVal;
 	return (
 
 			<form onSubmit={e => this.props.processGuess(e, this.state.userGuess)} >
-				<input className="text" id="userInput" value={this.state.userGuess} type="number" maxLength="3" required disabled={this.props.gameEnd}
+				<input className="text" id="userInput" value={inVal} type="number" maxLength="3" required disabled={this.props.gameEnd}
 				onChange={(e) => this.passIt(e.target.value)} ></input>
 				<input className="button" type="submit" disabled={this.props.gameEnd}></input>
 			</form>
